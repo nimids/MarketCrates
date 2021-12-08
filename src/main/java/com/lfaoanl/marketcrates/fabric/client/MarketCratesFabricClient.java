528f68ab633d4874ae5a1e3c42144ac6473ca2c0
lfaoanl <lfaoanl@gmail.com>
2021-12-08 12:14:39 +0100

    Added Fabric files

---
+++ src/main/java/com/lfaoanl/marketcrates/fabric/client/MarketCratesFabricClient.java
@@ -0,0 +1,65 @@
+package com.lfaoanl.marketcrates.fabric.client;
+
+import com.lfaoanl.marketcrates.fabric.blocks.AbstractCrateBlockEntity;
+import com.lfaoanl.marketcrates.common.ClientProxy;
+import com.lfaoanl.marketcrates.common.MarketCrates;
+import com.lfaoanl.marketcrates.fabric.core.CrateRegistry;
+import com.lfaoanl.marketcrates.fabric.gui.CrateDoubleScreen;
+import com.lfaoanl.marketcrates.fabric.gui.CrateScreen;
+import com.lfaoanl.marketcrates.render.CrateBlockEntityRenderer;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.client.rendereregistry.v1.BlockEntityRendererRegistry;
+import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.NonNullList;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+
+@Environment(EnvType.CLIENT)
+public class MarketCratesFabricClient extends ClientProxy implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        MarketCrates.proxy = this;
+
+        ScreenRegistry.register(CrateRegistry.CRATE_SCREEN, CrateScreen::new);
+        ScreenRegistry.register(CrateRegistry.CRATE_DOUBLE_SCREEN, CrateDoubleScreen::new);
+
+        BlockEntityRendererRegistry.INSTANCE.register(CrateRegistry.CRATE_BLOCK_ENTITY, CrateBlockEntityRenderer::new);
+
+        registerClientPacketReceiver();
+    }
+
+    private void registerClientPacketReceiver() {
+        ClientPlayNetworking.registerGlobalReceiver(CrateRegistry.CRATE_CHANNEL, (client, handler, buf, responseSender) -> {
+            MarketCrates.LOGGER.info("Client receiver initialised");
+
+            // Read packet data on the event loop
+            BlockPos target = buf.readBlockPos();
+            int itemsSize = buf.readInt();
+            NonNullList<ItemStack> items = NonNullList.create();
+            for (int i = 0; i < itemsSize; i++) {
+                items.add(i, buf.readItem());
+            }
+
+            client.execute(() -> {
+                // Everything in this lambda is run on the render thread
+                Level world = MarketCrates.proxy.getWorld();
+
+                if (world != null) {
+                    BlockEntity tile = world.getBlockEntity(target);
+
+                    if (tile instanceof AbstractCrateBlockEntity) {
+
+                        ((AbstractCrateBlockEntity) tile).receiveContents(items);
+                    }
+                }
+            });
+        });
+    }
+
+}
---
+++ src/main/java/com/lfaoanl/marketcrates/fabric/core/CrateRegistry.java
@@ -0,0 +1,82 @@
+package com.lfaoanl.marketcrates.fabric.core;
+
+import com.lfaoanl.marketcrates.Ref;
+import com.lfaoanl.marketcrates.fabric.blocks.AbstractCrateBlock;
+import com.lfaoanl.marketcrates.fabric.blocks.AbstractCrateBlockEntity;
+import com.lfaoanl.marketcrates.fabric.blocks.CrateBlock;
+import com.lfaoanl.marketcrates.fabric.blocks.CrateBlockEntity;
+import com.lfaoanl.marketcrates.fabric.gui.CrateContainer;
+import com.lfaoanl.marketcrates.fabric.gui.CrateDoubleContainer;
+import com.lfaoanl.marketcrates.items.CrateItem;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
+import net.fabricmc.fabric.api.screenhandler.v1.ScreenHandlerRegistry;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.NonNullList;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.inventory.MenuType;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+
+import java.util.HashMap;
+
+public class CrateRegistry {
+    public static final ResourceLocation CRATE_CHANNEL = new ResourceLocation(Ref.MODID, "crate_channel");
+
+//    public static final DeferredRegister<MenuType<?>> CONTAINERS = DeferredRegister.create(ForgeRegistries.CONTAINERS, References.MODID);
+//    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, References.MODID);
+//    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, References.MODID);
+//    public static final DeferredRegister<BlockEntityType<?>> TILES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITIES, References.MODID);
+//
+//
+//    public static final RegistryObject<MenuType<CrateContainer>> CONTAINER_CRATE = CONTAINERS.register("crate", () -> IForgeContainerType.create((windowId, inv, data) -> new CrateContainer(windowId, inv)));
+//    public static final RegistryObject<MenuType<CrateDoubleContainer>> CONTAINER_CRATE_DOUBLE = CONTAINERS.register("crate_double", () -> IForgeContainerType.create((windowId, inv, data) -> new CrateDoubleContainer(windowId, inv)));
+
+
+    public static String[] woodTypes = new String[]{"oak", "spruce", "birch", "jungle", "acacia", "dark_oak", "crimson", "warped"};
+
+//    public static final RegistryObject<BlockEntityType<CrateTileEntity>> CRATE_TILE = TILES.register("crate", () -> BlockEntityType.Builder.of(CrateTileEntity::new, CrateRegistry.validCrates()).build(null));
+
+    public static final HashMap<String, Item> items = new HashMap<>();
+    public static final HashMap<String, Block> blocks = new HashMap<>();
+
+    public static BlockEntityType<CrateBlockEntity> CRATE_BLOCK_ENTITY;
+
+    public static MenuType<CrateDoubleContainer> CRATE_DOUBLE_SCREEN;
+    public static MenuType<CrateContainer> CRATE_SCREEN;
+
+    public static void preInit() {
+        for (String type : woodTypes) {
+            AbstractCrateBlock block = new CrateBlock();
+            blocks.put(type, block);
+            items.put(type, new CrateItem(block, new FabricItemSettings().group(CreativeModeTab.TAB_DECORATIONS)));
+        }
+
+        CRATE_DOUBLE_SCREEN = ScreenHandlerRegistry.registerSimple(new ResourceLocation(Ref.MODID, "crate_double"), CrateDoubleContainer::new);
+        CRATE_SCREEN = ScreenHandlerRegistry.registerSimple(new ResourceLocation(Ref.MODID, "crate"), CrateContainer::new);
+
+    }
+
+    public static void init() {
+
+        for (String type : woodTypes) {
+            ResourceLocation resourceLocation = new ResourceLocation(Ref.MODID, type + "_crate");
+            Registry.register(Registry.BLOCK, resourceLocation, blocks.get(type));
+            Registry.register(Registry.ITEM, resourceLocation, items.get(type));
+        }
+
+        ResourceLocation rlCrateBlockEntity = new ResourceLocation(Ref.MODID, "crate");
+
+        Block[] blocksArray = blocks.values().toArray(new Block[blocks.size()]);
+
+        BlockEntityType<CrateBlockEntity> beBuilder = FabricBlockEntityTypeBuilder.create(CrateBlockEntity::new, blocksArray).build(null);
+        CRATE_BLOCK_ENTITY = Registry.register(Registry.BLOCK_ENTITY_TYPE, rlCrateBlockEntity, beBuilder);
+    }
+
+}
